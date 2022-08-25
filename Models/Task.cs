729be875fef30cc34f063e597959bcb98884d440
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2022-08-25 00:11:38 -0400

    Fuent API - Task

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,19 +1,14 @@
 namespace projectef.Models;
 
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 [Table("Task")]
 public class Task
 {
-    [Key]
     public Guid TaskId { get; set; }
 
-    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
 
-    [Required]
-    [MaxLength(200)]
     public string Title { get; set; }
 
     public string Description { get; set; }
@@ -24,7 +19,6 @@ public class Task
 
     public virtual Category Category { get; set; }
 
-    [NotMapped]
     public string Summary { get; set; }
 }
 
--- TasksContext.cs
+++ TasksContext.cs
@@ -17,9 +17,26 @@ public class TasksContext : DbContext
         {
             category.ToTable("Category");
             category.HasKey(c => c.CategoryId);
-            category.Property(c => c.Name).IsRequired().HasMaxLength(150);
+            category.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(150);
             category.Property(c => c.Description);
 
+        });
+
+        modelBuilder.Entity<Task>(task =>
+        {
+            task.ToTable("Task");
+            task.HasKey(t => t.TaskId);
+            task.HasOne(t => t.Category)
+                .WithMany(c => c.Categories)
+                .HasForeignKey(t => t.CategoryId);
+            task.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+            task.Property(t => t.Description);
+            task.Property(t => t.PriorityTask);
+            task.Property(t => t.CreationDate);
         });
     }
 }
